907e0fd2362b0a05707dfdd65cfb728362fa998f
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2014-10-23 20:45:21 +0200

    make more robust against pull errors

--- install.sh
+++ install.sh
@@ -7,16 +7,14 @@ log() {
   echo $* >> /dev/stderr
 }
 
+clone() {
+  ( cd $SNAP_CACHE_DIR/r-snap-texlive && git pull )
+}
+
 clone_or_pull() {
-  cd $SNAP_CACHE_DIR
-  if test -d r-snap-texlive/.git; then
-    log "Updating TeXlive"
-    cd r-snap-texlive
-    git pull
-  else
-    log "Cloning TeXlive"
-    rm -rf r-snap-texlive
-    git clone https://github.com/krlmlr/r-snap-texlive.git
+  if ! clone; then
+    rm -rf $SNAP_CACHE_DIR/r-snap-texlive
+    git clone https://github.com/krlmlr/r-snap-texlive.git $SNAP_CACHE_DIR/r-snap-texlive
   fi
 }
 
